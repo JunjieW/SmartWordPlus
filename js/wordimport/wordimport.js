b93c3f736013cd4a7a0ecaf96167c3562cc419f7
ranger_git <ranger_git@sina.com>
2014-09-19 14:57:59 +0800

    read words from TextArea

---
+++ js/wordimport/wordimport.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  
+  
+  $('#button_import').bind("click",importWords);
+ 
+	/*var orderKey = localStorage['_sys_orderkey'];
+	var order = localStorage['_sys_order'];
+	
+	var list = getAllWord();
+	var itemcount = list.length;
+	var words = sortWordbyField(orderKey, order);
+	
+		for (var k = 0; k < itemcount; k++) {
+		$("#div_words").append(words[k].word);
+		$("#div_words").append("<br>");	
+	}*/
+});
+
+function importWords() {
+  string = document.all.ta_words.value;
+  //alert(string);
+  wordArray = string.split('\n');
+ /* alert('string splited'+ string);
+  alert('wordArray = ' + wordArray);*/
+  //alert('wordArray.length()= '+ wordArray.length);
+  for(var i = 0; i < t.length; i++){
+    alert(wordArray[i]);
+        
+  }
+  
+  
+}
